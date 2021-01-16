830390504b7b8ea9d05db78e8829ed3830ae4b17
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 19:12:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,15 +3,18 @@
 # Print a custom greeting for each element
 
 def greet_characters(array)
+
   array.each do |greet_character|
     print "Hello #{greet_character}!"
 end
+
 end
 
 
-
+# Use `each_with_index` to enumerate over the provided array
+#
+# Print a numbered list of each element
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+
+
 end
